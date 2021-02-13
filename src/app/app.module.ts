7b5bd45950f1b95a94a4fb1b7cb8822ab529ef2b
Argarm <aarongrmrr@gmail.com>
2021-02-13 13:23:27 +0000

    router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { OverDirectiveDirective } from './over-directive.directive';
+import { OverDirectiveDirective } from './shared/over-directive.directive';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { MainViewComponent } from './main-view/main-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     OverDirectiveDirective,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    MainViewComponent
   ],
   imports: [
     BrowserModule,

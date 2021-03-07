75f89efc9477f53f611437ae0f1fef7816827c59
Argarm <aarongrmrr@gmail.com>
2021-03-07 17:55:00 +0000

    calendar adapts to service schedule

--- src/app/services/book/book.component.ts
+++ src/app/services/book/book.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
-import { CalendarView, CalendarEvent } from 'angular-calendar';
+import { CalendarView, CalendarEvent, DAYS_OF_WEEK } from 'angular-calendar';
 import { differenceInMinutes, startOfDay, startOfHour } from 'date-fns';
+import { concatMap } from 'rxjs/operators';
+import { AccountService } from 'src/app/helpers/services/account.service';
+import { ShopService } from 'src/app/helpers/services/shop.service';
+import { Service } from 'src/app/models/service.model';
 
 @Component({
   selector: 'app-book',
@@ -8,7 +12,7 @@ import { differenceInMinutes, startOfDay, startOfHour } from 'date-fns';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent {
- 
+  
   @ViewChild('scrollContainer') scrollContainer: ElementRef<HTMLElement>;
 
   view: CalendarView = CalendarView.Day;
@@ -17,14 +21,60 @@ export class BookComponent {
 
   events: CalendarEvent[] = [];
 
-  constructor(private cdr: ChangeDetectorRef) {}
+  weekStartsOn = DAYS_OF_WEEK.MONDAY
+
+  todaySchedule = {
+    isOpen : false,
+    dayStart : 0,
+    dayEnd : 0
+  };
+  actualService: Service;
+  
+  constructor(private cdr: ChangeDetectorRef, private accountService: AccountService, private shopService : ShopService) {
+  }
 
   ngAfterViewInit() {
     this.scrollToCurrentView();
+    var collection = this.shopService.getCollection();
+    var document = this.shopService.getDocument();
+    this.accountService.getService(collection,document).subscribe((serviceSnapshot) => {
+      this.actualService = <Service>serviceSnapshot.data()
+    });
   }
 
-  viewChanged() {
+  private fillInformation( service: Service) {
+    var schedule = service.schedule.split('/').map(function(item){
+      item = item.trim()
+      var day = {
+        isOpen : true,
+        dayStart: 0,
+        dayEnd: 0 
+      }
+
+      if(item.indexOf("-")===-1){
+        day.isOpen=false
+      }else{
+        var todaySchedule = item.split(" ")[1].split("-")
+        var todayStart = todaySchedule[0].split(":")[0]
+        var todayEnd = todaySchedule[1].split(":")[0]
+        day.dayStart = Number(todayStart)
+        day.dayEnd = Number(todayEnd)
+        day.isOpen = true
+      }
+      return day
+    })
+    var today = (this.viewDate.getDay()+6)% 7
+    this.setTodaySchedule(schedule, today) 
+  }
+
+  setTodaySchedule(schedule: { isOpen: boolean; dayStart: number; dayEnd: number; }[], today: number) {
+    this.todaySchedule = schedule[today]
+  }
+
+  viewChanged() {  
     this.cdr.detectChanges();
+    this.fillInformation(this.actualService)
+    console.log(this.todaySchedule)
     this.scrollToCurrentView();
   }
 
